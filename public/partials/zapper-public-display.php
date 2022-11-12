a34565373df2268bf75ad16667ec32c5ac23066f
ajaxStardust <ajaxStardust@gmail.com>
2022-11-12 18:40:31 -0500

    initialize public repo
    initialize

---
+++ public/partials/zapper-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://whatsonyourbrain.com
+ * @since      1.0.0
+ *
+ * @package    Zapper
+ * @subpackage Zapper/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
